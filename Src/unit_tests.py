40089071bc803051945f6caf7766618b99b9b768
track394 <track394@yahoo.com>
2019-02-18 13:03:13 -0800

    Got unit tests working.

--- Src/unit_tests.py
+++ Src/unit_tests.py
@@ -2,19 +2,17 @@
 
 import unittest
 import data_hasher
-# import test_file
 
 
 class TestModules(unittest.TestCase):
-    """Test all modules in project"""
+    """Test all modules in project."""
 
     def test_data_hasher(self):
         """Test data hasher."""
         test_string = "This is a test string for basic hashing."
 
-        result = data_hasher.hash(test_string)
         self.assertEqual(
-            result,
+            data_hasher.hash(test_string),
             "c7901b91c1c0be6b1ad74faabb5f57701914e709952be9071a1323732fc6d7ea8feceaddf934e1ca35f1f0c33714b7cb3292bb7b9e6f2cd92821f93491a925bb"
         )
 
