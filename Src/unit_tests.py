631b16f5573536a188b10bf756cc4320e76db5a1
JohnHardy5 <track394@yahoo.com>
2019-02-27 18:54:34 -0800

    Got more tests working, starting on command-line file reading

--- Src/unit_tests.py
+++ Src/unit_tests.py
@@ -5,6 +5,7 @@ import data_hasher
 
 def to_binary(string):
     """Convert string to binary"""
+    print(''.join(format(x, 'b') for x in bytearray(string, "utf-8")))
     return ''.join(format(x, 'b') for x in bytearray(string, "utf-8"))
 
 class TestModules(unittest.TestCase):
@@ -12,13 +13,20 @@ class TestModules(unittest.TestCase):
 
     def test_hasher(self):
         """Test non-chunk based data hasher."""
+        empty_string = ""
         test_string = "this is a test string"
         test_bytes = to_binary("this is a test string")
+
+        self.assertEqual(
+            data_hasher.hash_string(empty_string),
+            "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26" # pylint: disable=line-too-long
+        )
 
         self.assertEqual(
             data_hasher.hash_string(test_string),
             "a88e43eb3b3627f9864679ece6b613497caa2f55f5efa309fe02e964b7ead7722e1bba710ed746d89962f84636c21a9a2a02a6c89d1fd99f9ac6a6fef7e6a32a" # pylint: disable=line-too-long
         )
+
         self.assertEqual(
             data_hasher.hash_string(test_bytes),
             "47c34fa5a116e04a9fe89e610ec5ee319cbccd6fc440e7f0b0bccd14d6593a7af9e910b07d5af44861283c29e67e79322a55d83333de0991d349e6f80acd5b64" # pylint: disable=line-too-long
@@ -26,8 +34,14 @@ class TestModules(unittest.TestCase):
 
     def test_chunk_hasher(self):
         """Test chunk based data hashing."""
+        empty = ""
         lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." # pylint: disable=line-too-long
         chunk_size = 8
+
+        self.assertEqual(
+            data_hasher.hash_in_chunks(empty, chunk_size),
+            "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26" # pylint: disable=line-too-long
+        )
 
         self.assertEqual(
             data_hasher.hash_in_chunks(lorem, chunk_size),
@@ -37,6 +51,7 @@ class TestModules(unittest.TestCase):
     def test_file_hasher(self):
         """Test file hashing on simple files as well as large ones."""
         text_file_path = "test_file.txt"
+        empty_file_path = "empty_file.txt"
         doc_file_path = "test_file.docx"
         mp4_file_path = "test_file.mp4"
 
@@ -44,6 +59,13 @@ class TestModules(unittest.TestCase):
         self.assertEqual(
             data_hasher.hash_file(file),
             "ef90bed878786313589b4c1458cf19b5cd8d0059239b3dd726ca8ba1bc803f361830c78c764f91163624f5b4e9d77435adc200e7a692be28b0f76680c3ee3e67" # pylint: disable=line-too-long
+        )
+        file.close()
+
+        file = open(empty_file_path, "rb")
+        self.assertEqual(
+            data_hasher.hash_file(file),
+            "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26" # pylint: disable=line-too-long
         )
         file.close()
 
